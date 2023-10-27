fde008b3179955648b04d86363046cce93cc1779
Derek <derekdanielson84@gmail.com>
2023-10-27 12:26:04 -0600

    Added HTML elements

---
+++ app.js
@@ -0,0 +1,8 @@
+// Bring in elements from HTML
+const buttonEl = document.querySelectorAll('button');
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+const resultEl = document.getElementById('result');
+const userScore = document.getElementById('user-score');
+const computerScore = document.getElementById('computer-score');
