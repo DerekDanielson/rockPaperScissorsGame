2234284317b2320d25b559c157991fb5e8e8d9fd
Derek <derekdanielson84@gmail.com>
2023-10-27 12:27:01 -0600

    Added array of choices

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const scissorsBtn = document.getElementById('scissors');
 const resultEl = document.getElementById('result');
 const userScore = document.getElementById('user-score');
 const computerScore = document.getElementById('computer-score');
+
+let arr = ['&#x1F44A;','&#x1F590;','&#x270C;'];
+
+
